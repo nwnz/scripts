bbf389cd7130328d4b5b73f15703d12532d8837d
Nikita Ostapenko <nwnzer@gmail.com>
2012-03-10 22:53:11 +0600

    Добавил обнуление второго интерфейса

--- change_IP.sh
+++ change_IP.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo ifconfig eth0 192.168.0.2 netmask 255.255.255.0
+sudo ifconfig eth0:0 0.0.0.0
 sudo route add default gw 192.168.0.1
 echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf
 
